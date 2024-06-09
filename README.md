d1711f38deb13d3451379570b947c517bfd1b0fe
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-06-09 22:13:05 +0000

    done

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# ICD2O Unit #X-YY
+# ICD2O Unit #5-03
 
 [![Mr Coxall's Super Linter](README.md/../../../workflows/Super%20Linter/badge.svg)](README.md/../../../actions)
 
 [![Deploy Pages](README.md/../../../workflows/Deploy%20Pages/badge.svg)](README.md/../../../actions)
 
-This site can be found at: <link>
+This site can be found at: https://icd2o-digital-tech-atrisarker.github.io/Unit5-03-HTML-Factorial/
--- js/script.js
+++ js/script.js
@@ -1,5 +1,46 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Script for Factorial of a number
+
+// Get Elements
+
+// Inputs
+const userNumInput = document.getElementById("userNumInput");
+
+// Input Form
+const inputForm = document.getElementById("inputForm");
+
+
+function getResult() {
+
+  // Get userNum, limit of 172 as any factorial past that will evaluate to Infinity
+  let userNum = Math.min(Number(userNumInput.value), 172);
+
+  // Initalize Factorial Product
+  let product = 1;
+
+  // Initialize counter
+  let counter = 1;
+
+  // Do While Loop to calculate factorial
+  do {
+    
+    // Amplify/Multiply product by counter
+    product *= counter;
+    
+    // Increment Counter
+    counter += 1;
+    
+  } while (counter <= userNum);
+
+  // Display Result
+  outputResult.innerHTML = `${product}`;
+
+  return;
+}
+
+
+// Connect form submit to getResult()
+inputForm.onsubmit = () => {
+  getResult();
+  // Prevents page from refreshing
+  return false;
+}
